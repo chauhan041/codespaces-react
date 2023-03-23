a16d0764f06f4e25e5126d7ffe7ee6ecf40412b4
chauhan041 <chauhansumit44@gmail.com>
2023-03-23 09:04:05 +0000

    add components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  let {title, description, imageUrl} = this.props
+  render() {
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Read more</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
